771aaa44a7a9bd527bc2152985458eab123454c3
Nikita <nikitosminsk100@gmail.com>
2021-03-09 19:02:25 +0300

    [LR2]: Feat(Task 2.2.6 with sinus - done. + makefile)

---
+++ LR2/Z2.2_V6/cmake-build-debug/CMakeFiles/Z2_2_V6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Z2_2_V6.dir/main.c.obj"
+  "Z2_2_V6.exe"
+  "Z2_2_V6.exe.manifest"
+  "Z2_2_V6.pdb"
+  "libZ2_2_V6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Z2_2_V6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
